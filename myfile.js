b1df37c836d82c737b8f406e876d09801ddcbbc7
ishida tsubasa <hiphop19930613@gmail.com>
2020-03-02 23:21:38 +0900

    配列をソートする処理を追加しました

--- myfile.js
+++ myfile.js
@@ -3,3 +3,16 @@
 
 
 んｋｂｊんｋｓｈ：あぇぁｋぇｈｊｂｓ；ふぇ：あ；ｂｈｈｊｆｊはヵｈｌ：ｂ
+
+var sortNumber = function (number) {
+   number.sort(function (a, b) {
+       if (a == b) {
+           return 0;
+       }
+       return a < b ? -1 : 1;
+   });
+};
+
+var number = [19, 3, 81, 1, 24, 21];
+sortNumber(number);
+console.log(number);
